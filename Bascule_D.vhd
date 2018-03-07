8dea6014c752cd96d5bfefd187556a90fce4d41b
Johann Zschau <zschau.j@hotmail.fr>
2018-03-07 16:03:54 +0100

    Message de validation

---
+++ Bascule_D.vhd
@@ -0,0 +1,44 @@
+-- Copyright (C) 2017  Intel Corporation. All rights reserved.
+-- Your use of Intel Corporation's design tools, logic functions 
+-- and other software and tools, and its AMPP partner logic 
+-- functions, and any output files from any of the foregoing 
+-- (including device programming or simulation files), and any 
+-- associated documentation or information are expressly subject 
+-- to the terms and conditions of the Intel Program License 
+-- Subscription Agreement, the Intel Quartus Prime License Agreement,
+-- the Intel FPGA IP License Agreement, or other applicable license
+-- agreement, including, without limitation, that your use is for
+-- the sole purpose of programming logic devices manufactured by
+-- Intel and sold by Intel or its authorized distributors.  Please
+-- refer to the applicable agreement for further details.
+
+library ieee;
+use ieee.std_logic_1164.all;
+library altera;
+use altera.altera_syn_attributes.all;
+
+entity Bascule_D is
+	port
+	(
+-- {ALTERA_IO_BEGIN} DO NOT REMOVE THIS LINE!
+
+		clk : in std_logic;
+		d : in std_logic;
+		q : out std_logic
+-- {ALTERA_IO_END} DO NOT REMOVE THIS LINE!
+
+	);
+
+-- {ALTERA_ATTRIBUTE_BEGIN} DO NOT REMOVE THIS LINE!
+-- {ALTERA_ATTRIBUTE_END} DO NOT REMOVE THIS LINE!
+end Bascule_D;
+
+architecture ppl_type of Bascule_D is
+
+-- {ALTERA_COMPONENTS_BEGIN} DO NOT REMOVE THIS LINE!
+-- {ALTERA_COMPONENTS_END} DO NOT REMOVE THIS LINE!
+begin
+-- {ALTERA_INSTANTIATION_BEGIN} DO NOT REMOVE THIS LINE!
+-- {ALTERA_INSTANTIATION_END} DO NOT REMOVE THIS LINE!
+
+end;
---
+++ output_files/Bascule_D.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 17.1.0 Build 590 10/25/2017 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10M50DAF484) Path("C:/Users/zscha/Documents/VHDL/output_files/") File("Bascule_D.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
